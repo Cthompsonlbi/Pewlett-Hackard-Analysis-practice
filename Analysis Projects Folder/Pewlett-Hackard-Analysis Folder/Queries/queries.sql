9cc203f1cd2dfdd04a1dd0a7b27a1e604aa0440a
Chad Thompson <Chad.Thompson@Qorvo.com>
2021-11-28 18:24:35 -0500

    completed mod 7 exercises

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/queries.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/queries.sql
@@ -38,3 +38,160 @@ WHERE (birth_date Between '1952-01-01' AND '1955-12-31')
 AND (hire_date Between '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+--Queries created for 7.3.2 Types of Joins
+
+--Drop Retirement_info table
+DROP TABLE retirement_info;
+
+--Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE ( birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-1-01'AND '1988-12-31');
+
+--Check the table
+SELECT * FROM retirement_info;
+
+--7.3.3 Joins In Action
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+--Using Aliases to shorten queries.  The belwo is the same as above but using aliases
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+--Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+--Using Aliases to shorten queries.  The belwo is the same as above but using aliases
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01')
+
+--7.3.4 use count, group by and order by
+--employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retirement_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+--7.3.5 Create Additional Lists
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no, first_name, last_name, gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+DROP TABLE emp_info;
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+	 
+ --List of managers per department
+ SELECT dm.dept_no,
+ 	d.dept_name,
+	dm.emp_no, 
+ 	ce.first_name, 
+	ce.last_name,
+	dm.from_date, 
+	dm.to_date
+INTO manager_info	
+FROM dept_manager as dm
+	INNER JOIN departments as d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp AS ce
+		ON (dm.emp_no = ce.emp_no);
+		
+--Deparment Retirees
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON(ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+--7.3.6 Tailored lists
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	--de.dept_no,
+	d.dept_name
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON(ri.emp_no = de.emp_no)
+INNER JOIN departments as d
+On(de.dept_no = d.dept_no)
+WHERE dept_name = ('Sales')
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	--de.dept_no,
+	d.dept_name
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON(ri.emp_no = de.emp_no)
+INNER JOIN departments as d
+On(de.dept_no = d.dept_no)
+WHERE dept_name IN ('Sales', 'Development')
+ORDER BY d.dept_name;
+
+
+	
+	
